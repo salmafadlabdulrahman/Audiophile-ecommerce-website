e1e6d0c3414021fb4b5f08263ee8c58d17ca5449
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-13 12:45:15 +0200

    add products to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,10 @@ import { Link } from "react-router-dom";
 import arrowRight from "/assets/shared/desktop/icon-arrow-right.svg";
 import headPhoneImg from "/assets/shared/desktop/image-headphones.png";
 import speakerImg from "/assets/shared/desktop/image-speakers.png";
+import speakerProductImg from "/assets/home/mobile/image-speaker-zx9.png";
 import earphonesImg from "/assets/shared/desktop/image-earphones.png";
+import speakerImgMobile from "/assets/home/mobile/image-speaker-zx7.jpg";
+import earphoneImgMobile from "/assets/home/mobile/image-earphones-yx1.jpg";
 
 function Home() {
   return (
@@ -28,7 +31,11 @@ function Home() {
         <main className="main">
           <div className="main-container">
             <div className="shop-item-container">
-              <img src={headPhoneImg} alt="headphones" className="product-img"/>
+              <img
+                src={headPhoneImg}
+                alt="headphones"
+                className="product-img"
+              />
               <h3>Headphones</h3>
               <Link>
                 Shop{" "}
@@ -38,7 +45,7 @@ function Home() {
               </Link>
             </div>
             <div className="shop-item-container">
-              <img src={speakerImg} alt="headphones" className="product-img"/>
+              <img src={speakerImg} alt="headphones" className="product-img" />
               <h3>Speakers</h3>
               <Link>
                 Shop{" "}
@@ -48,7 +55,11 @@ function Home() {
               </Link>
             </div>
             <div className="shop-item-container">
-              <img src={earphonesImg} alt="headphones" className="product-img"/>
+              <img
+                src={earphonesImg}
+                alt="headphones"
+                className="product-img"
+              />
               <h3>Earphones</h3>
               <Link>
                 Shop{" "}
@@ -56,6 +67,46 @@ function Home() {
                   <img src={arrowRight} />
                 </span>
               </Link>
+            </div>
+
+            <div className="thumbnails-container">
+              <div className="new-product-container">
+                <div className="product-content">
+                  <img src={speakerProductImg} alt="speaker Image" />
+                  <h2>
+                    ZX9 <br />
+                    SPEAKER
+                  </h2>
+                  <p>
+                    Upgrade to premium speakers that are phenomenally built to
+                    deliver truly remarkable sound.
+                  </p>
+                  <button>See Product</button>
+                </div>
+              </div>
+
+              <div className="new-product-details-container">
+                <div className="product-display">
+                  <img src={speakerImgMobile} alt="speaker Image" />
+                  <div className="product-display-content">
+                    <h2>ZX7 SPEAKER</h2>
+                    <button>See Product</button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="new-item-container">
+                <div className="new-product-img">
+                  <img src={earphoneImgMobile} alt="earphone Image" />
+                </div>
+
+                <div className="new-product-info">
+                  <div className="info">
+                    <h2>Yx1 earphones</h2>
+                    <button>See Product</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </main>

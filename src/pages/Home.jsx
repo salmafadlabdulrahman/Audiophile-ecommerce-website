df2de6624fbbe9483fe19ef6762f695ba9e785aa
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-13 10:26:56 +0200

    add products to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,32 +28,32 @@ function Home() {
         <main className="main">
           <div className="main-container">
             <div className="shop-item-container">
-              <img src={headPhoneImg} alt="headphones" />
+              <img src={headPhoneImg} alt="headphones" className="product-img"/>
               <h3>Headphones</h3>
               <Link>
                 Shop{" "}
                 <span>
-                  {/*<img src={arrowRight} />*/}
+                  <img src={arrowRight} />
                 </span>
               </Link>
             </div>
             <div className="shop-item-container">
-              <img src={speakerImg} alt="headphones" />
+              <img src={speakerImg} alt="headphones" className="product-img"/>
               <h3>Speakers</h3>
               <Link>
                 Shop{" "}
                 <span>
-                  {/*<img src={arrowRight} />*/}
+                  <img src={arrowRight} />
                 </span>
               </Link>
             </div>
             <div className="shop-item-container">
-              <img src={earphonesImg} alt="headphones" />
+              <img src={earphonesImg} alt="headphones" className="product-img"/>
               <h3>Earphones</h3>
               <Link>
                 Shop{" "}
                 <span>
-                  {/*<img src={arrowRight} />*/}
+                  <img src={arrowRight} />
                 </span>
               </Link>
             </div>

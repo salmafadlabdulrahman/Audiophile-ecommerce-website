07489388a7ea108973309c26ac4dd5dd93c738c6
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-15 13:33:11 +0200

    create headphones page

--- src/pages/HeadPhones.jsx
+++ src/pages/HeadPhones.jsx
@@ -1,9 +1,38 @@
+import "../styles/headphones.css";
+
+import Allproducts from "../../products.json";
+//console.log(Allproducts.products)
+
+//products.map(product => console.log(product))
+
+const headphones = Allproducts.products.filter((product) =>
+  product.slug.includes("headphones")
+);
+
 function HeadPhones() {
   return (
-    <div>
-      <h1>Headphones page</h1>
+    <div className="headphone">
+      <div className="headphones-wrapper">
+        <div className="page-name-container">
+          <h1>Headphones</h1>
+        </div>
+
+        <div className="category-products-container">
+          <div className="product-container">
+            {headphones.map((headphone, index) => (
+              <div className="product" key={index}>
+                <img src={headphone.categoryImage.mobile} />
+                <h4>New Product</h4>
+                <h3>{headphone.name}</h3>
+                <p>{headphone.description}</p>
+                <button>see product</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeadPhones
+export default HeadPhones;
